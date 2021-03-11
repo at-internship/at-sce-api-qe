afe3b16174a96252230131569916f3e1edf5447f
Kevin Castillo Roa <78884918+KevinQAstillo@users.noreply.github.com>
2021-03-11 11:50:46 -0600

    Add lombok dependency

--- src/test/java/com/at/globalclasses/domain/UserRequest.java
+++ src/test/java/com/at/globalclasses/domain/UserRequest.java
@@ -1,27 +1,13 @@
 package com.at.globalclasses.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class UserRequest {
 
 	private String email;
 	private String password;
-	
-	public UserRequest() {
-		super();
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	
+		
 }
