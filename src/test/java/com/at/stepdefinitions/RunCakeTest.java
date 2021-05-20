94279a729a00523c9304f958c5420a74448158a6
EmmanuelQAstaneda <emmanuelcastaneda@agilethought.com>
2021-05-20 17:25:43 -0500

    Numero 7

--- src/test/java/com/at/stepdefinitions/RunCakeTest.java
+++ src/test/java/com/at/stepdefinitions/RunCakeTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         "json:target/cucumber-reports/cucumber.json"},
         features = {"src/test/resources/"},
         glue = {"com.at.stepdefinitions"},
-        tags = "@US_048 and not @Deprecated",
+        tags = "@Get and @32 and not @Deprecated",
         monochrome = false)
 public class RunCakeTest {
 }
